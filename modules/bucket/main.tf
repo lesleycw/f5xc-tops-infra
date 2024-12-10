147c63d5067f23b5ba92efa352c06e5ef9ee4904
Kevin Reynolds <kevin@k11s.io>
2024-12-10 10:42:45 -0500

    removed extra var

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS region for the S3 bucket"
-  type        = string
-  default     = ""
-}
-
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
