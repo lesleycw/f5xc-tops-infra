34466b82e7784192b84c3a68abd5eb7202f536c5
Kevin Reynolds <kevin@k11s.io>
2024-12-09 19:42:38 -0500

    create new branch

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "remote" {
+    organization = "f5xc-tenant-ops"
+
+    workspaces {
+      name = "tops-infra-dev"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket        = var.bucket_name
+  acl           = "private"
+  force_destroy = true
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
