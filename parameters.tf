ba076f5675f77bce31a5641c6ed6c794a10f74bb
Kevin Reynolds <kevin@k11s.io>
2024-12-15 20:59:25 -0500

    add parameters

---
+++ parameters.tf
@@ -0,0 +1,65 @@
+locals {
+  app_base_path = "/tenantOps-${var.environment}/app-lab"
+  mcn_base_path = "/tenantOps-${var.environment}/mcn-lab"
+  sec_base_path = "/tenantOps-${var.environment}/sec-lab"
+}
+
+module "parameter_store" {
+  source = "./modules/parameter-store"
+
+  parameters = {
+    "${local.app_base_path}/tenant-url" = {
+      type        = "String"
+      value       = "https://f5-xc-lab-app.console.ves.volterra.io"
+    }
+    "${local.app_base_path}/token-name" = {
+      type        = "String"
+      value       = "tenant-ops-abkdibjd"
+    }
+    "${local.app_base_path}/token-value" = {
+      type        = "SecureString"
+      value       = var.app_lab_token
+    }
+  }
+  default_tags    = local.tags
+}
+
+module "parameter_store" {
+  source = "./modules/parameter-store"
+
+  parameters = {
+    "${local.mcn_base_path}/tenant-url" = {
+      type        = "String"
+      value       = "https://f5-xc-lab-mcn.console.ves.volterra.io"
+    }
+    "${local.mcn_base_path}/token-name" = {
+      type        = "String"
+      value       = "tenant-ops-mrflfitl"
+    }
+    "${local.mcn_base_path}/token-value" = {
+      type        = "SecureString"
+      value       = var.mcn_lab_token
+    }
+  }
+  default_tags    = local.tags
+}
+
+module "parameter_store" {
+  source = "./modules/parameter-store"
+
+  parameters = {
+    "${local.sec_base_path}/tenant-url" = {
+      type        = "String"
+      value       = "https://f5-xc-lab-sec.console.ves.volterra.io"
+    }
+    "${local.sec_base_path}/token-name" = {
+      type        = "String"
+      value       = "tenant-ops-rcsodtbj"
+    }
+    "${local.sec_base_path}/token-value" = {
+      type        = "SecureString"
+      value       = var.sec_lab_token
+    }
+  }
+  default_tags    = local.tags
+}
