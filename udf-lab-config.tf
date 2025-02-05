fc2ff3bd0738a165db76e67e32aab26acbdc57cf
Kevin Reynolds <kevin@k11s.io>
2025-02-04 20:24:34 -0500

    test lab entry

--- udf-lab-config.tf
+++ udf-lab-config.tf
@@ -12,3 +12,24 @@ resource "aws_dynamodb_table" "lab_configuration" {
 /*
 Individual Lab Configs here
 */
+
+resource "aws_dynamodb_table_item" "lab_cMIxKy" {
+  table_name = aws_dynamodb_table.lab_configuration.name
+  hash_key   = "lab_id"
+
+  item = jsonencode({
+    lab_id          = { S = "cMIxKy" }
+    description     = { S = "Lab for testing" }
+    ssm_base_path   = { S = "${local.sec_base_path}" }
+    sso_type        = { S = "SSO" }
+    group_names     = { L = ["xc-lab-users"] }
+    namespace_roles = { L = [
+      { M = {
+        namespace = { S = "system" }
+        role      = { S = "f5xc-web-app-scanning-user" }
+      }}
+    ]}
+    pre_lambda      = { S = "arn:aws:lambda:us-west-2:123456789012:function:pre-lambda" }
+    post_lambda     = { NULL = true }
+  })
+}
